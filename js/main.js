18477fcff66c19de69ad96175423781c7c7692e4
Tim <tim@tim-ings.com>
2018-01-28 05:46:11 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,80 @@
+/*
+Written by Tim Ings <tim@tim-ings.com>
+Copyright 2018 All rights reserved
+ */
+
+function ping(host, callback) {
+    var timeStart = new Date().getTime();
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://" + host + ":" + 80, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            callback(new Date().getTime() - timeStart);
+        }
+    };
+    try {
+        xhr.send(null);
+    } catch (exception) {
+    }
+}
+
+function buildTable() {
+    var tableData = []
+    for (var i = 0; i < worldData.length; i++) {
+        var world = worldData[i];
+        var row = {
+            "world": world["id"],
+            "type": world["type"],
+            "members": world["members"],
+            "location": world["location"],
+            "latency": "",
+        };
+        tableData.push(row);
+    }
+    $("#tb-ping-results").bootstrapTable({
+        search: true,
+        striped: true,
+        columns: [{
+            sortable: true,
+            field: "world",
+            title: "World",
+            class: "col-world",
+        }, {
+            sortable: true,
+            field: "type",
+            title: "Type",
+            class: "col-type",
+        }, {
+            sortable: true,
+            field: "members",
+            title: "Members",
+            class: "col-members",
+        }, {
+            sortable: true,
+            field: "location",
+            title: "Location",
+            class: "col-location",
+        }, {
+            sortable: true,
+            field: "latency",
+            title: "Latency",
+            class: "col-latency",
+        }],
+        data: tableData,
+        onClickRow: function (row, element, field) {
+            var worldID = row["world"];
+            var worldUrl = "oldschool" + (parseInt(worldID) - 300) + ".runescape.com";
+            ping(worldUrl, function (timeElapsed) {
+                $("#tb-ping-results").bootstrapTable("updateCell", {
+                    index: element[0].dataset.index,
+                    field: "latency",
+                    value: timeElapsed / 2,
+                });
+            })
+        },
+    });
+}
+
+$(document).ready(function () {
+    buildTable();
+});
---
+++ js/worldData.js
@@ -0,0 +1,560 @@
+/*
+Written by Tim Ings <tim@tim-ings.com>
+Copyright 2018 All rights reserved
+ */
+
+var worldData = [
+    // ----------------------------------------- Col 1 ----------------------------------------- 
+    {
+        "id": 301,
+        "type": "Trade - Free",
+        "members": false,
+        "location": "US"
+    },
+    {
+        "id": 302,
+        "type": "Trade - Members",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 303,
+        "type": "-",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 304,
+        "type": "Trouble Brewing",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 305,
+        "type": "Falador Party Room",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 306,
+        "type": "Barbarian Assault",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 307,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 308,
+        "type": "Wilderness PK - free style",
+        "members": false,
+        "location": "GB"
+    },
+    {
+        "id": 309,
+        "type": "Wintertodt",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 310,
+        "type": "Kourend Group Activity",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 311,
+        "type": "Chambers of Xeric",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 312,
+        "type": "Treasure Trails",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 313,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 314,
+        "type": "Dorgesh-Kaan Agility",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 315,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 316,
+        "type": "Wilderness PK - free style",
+        "members": false,
+        "location": "GB"
+    },
+    {
+        "id": 317,
+        "type": "LMS Competitive",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 318,
+        "type": "Bounty World",
+        "members": true,
+        "location": "GB"
+    },
+
+
+
+
+
+    // ----------------------------------------- Col 2 ----------------------------------------- 
+    {
+        "id": 319,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 320,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 321,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 322,
+        "type": "Duel Arena",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 323,
+        "type": "Volcanic Mine",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 324,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 325,
+        "type": "PVP World",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 326,
+        "type": "LMS Casual",
+        "members": false,
+        "location": "GB"
+    },
+    {
+        "id": 327,
+        "type": "Ourania Altar",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 328,
+        "type": "-",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 329,
+        "type": "Clan Wars - Members",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 330,
+        "type": "House Party, Gilded Altar",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 331,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 332,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 333,
+        "type": "Games Room, Rogue's Den",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 334,
+        "type": "Castle Wars 1",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 335,
+        "type": "-",
+        "members": false,
+        "location": "DE"
+    },
+    {
+        "id": 336,
+        "type": "Running - nature runes",
+        "members": true,
+        "location": "DE"
+    },
+    
+
+
+
+
+    // ----------------------------------------- Col 3 ----------------------------------------- 
+    {
+        "id": 337,
+        "type": "PVP World - High Risk",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 338,
+        "type": "Chambers of Xeric",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 339,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 340,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 341,
+        "type": "Running - law runes",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 342,
+        "type": "Role-playing",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 343,
+        "type": "-",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 344,
+        "type": "Pest Control",
+        "members": false,
+        "location": "DE"
+    },
+    {
+        "id": 345,
+        "type": "Deadman",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 346,
+        "type": "Agility training",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 347,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 348,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 349,
+        "type": "2000 skill total",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 350,
+        "type": "TzHaar Fight Pit",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 351,
+        "type": "-",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 352,
+        "type": "Blast Furnace",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 353,
+        "type": "1250 skill total",
+        "members": false,
+        "location": "US"
+    },
+    {
+        "id": 354,
+        "type": "Castle Wars 2",
+        "members": true,
+        "location": "US"
+    },
+    
+
+
+
+
+    // ----------------------------------------- Col 4 ----------------------------------------- 
+    {
+        "id": 355,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 356,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 357,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 358,
+        "type": "Blast Furnace",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 359,
+        "type": "-",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 360,
+        "type": "-",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 361,
+        "type": "2000 skill total",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 362,
+        "type": "Pyramid Plunder",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 365,
+        "type": "High Risk World",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 366,
+        "type": "1500 skill total",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 367,
+        "type": "Group Questing",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 368,
+        "type": "-",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 369,
+        "type": "Wilderness PK - members",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 370,
+        "type": "Fishing Trawler",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 373,
+        "type": "1750 skill total",
+        "members": true,
+        "location": "GB"
+    },
+    {
+        "id": 374,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 375,
+        "type": "Barbarian Fishing",
+        "members": true,
+        "location": "DE"
+    },
+    {
+        "id": 376,
+        "type": "-",
+        "members": true,
+        "location": "DE"
+    },
+    
+
+
+
+
+    // ----------------------------------------- Col 5 ----------------------------------------- 
+    {
+        "id": 377,
+        "type": "Mor'ton temple, Rat Pits",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 378,
+        "type": "-",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 381,
+        "type": "500 skill total",
+        "members": false,
+        "location": "GB"
+    },
+    {
+        "id": 382,
+        "type": "-",
+        "members": false,
+        "location": "GB"
+    },
+    {
+        "id": 383,
+        "type": "Castle Wars - free",
+        "members": false,
+        "location": "DE"
+    },
+    {
+        "id": 384,
+        "type": "-",
+        "members": false,
+        "location": "DE"
+    },
+    {
+        "id": 385,
+        "type": "750 skill total",
+        "members": false,
+        "location": "US"
+    },
+    {
+        "id": 386,
+        "type": "Blast Furnace",
+        "members": true,
+        "location": "US"
+    },
+    {
+        "id": 387,
+        "type": "Blast Furnace",
+        "members": true,
+        "location": "AU"
+    },
+    {
+        "id": 388,
+        "type": "-",
+        "members": true,
+        "location": "AU"
+    },
+    {
+        "id": 389,
+        "type": "-",
+        "members": true,
+        "location": "AU"
+    },
+    {
+        "id": 390,
+        "type": "-",
+        "members": true,
+        "location": "AU"
+    },
+    {
+        "id": 391,
+        "type": "1750 skill total",
+        "members": true,
+        "location": "AU"
+    },
+    {
+        "id": 392,
+        "type": "PVP World",
+        "members": true,
+        "location": "AU"
+    },
+    {
+        "id": 393,
+        "type": "Clan Wars - Free",
+        "members": false,
+        "location": "US"
+    },
+    {
+        "id": 394,
+        "type": "-",
+        "members": false,
+        "location": "US"
+    }
+];
