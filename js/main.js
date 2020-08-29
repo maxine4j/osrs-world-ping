affc5b4e5204b3b1631afdd3af5cecd61d4ac4d7
Tim Ings <tim@tim-ings.com>
2020-08-29 20:02:09 +0800

    All pings are sent on page load, and adds throbber

--- js/main.js
+++ js/main.js
@@ -1,60 +1,94 @@
-const ping = (host) => {
-    return new Promise(async (resolve) => {
-        const timeStart = new Date().getTime();
-        await fetch(`${location.protocol}//${host}`, { mode: 'no-cors' });
-        resolve(new Date().getTime() - timeStart);
-    });
+const columns = [{
+    sortable: true,
+    field: "world",
+    title: "World",
+    class: "col-world",
+}, {
+    sortable: true,
+    field: "type",
+    title: "Type",
+    class: "col-type d-none d-md-table-cell",
+}, {
+    sortable: true,
+    field: "members",
+    title: "Members",
+    class: "col-members d-none d-md-table-cell",
+}, {
+    sortable: true,
+    field: "location",
+    title: "Location",
+    class: "col-location",
+}, {
+    sortable: true,
+    field: "latency",
+    title: "Latency",
+    class: "col-latency",
+}];
+
+const data = worldData.map((w) => ({
+    world: w["id"],
+    type: w["type"],
+    members: w["members"],
+    location: w["location"],
+    latency: "",
+}));
+
+const ping = async (host) => {
+    const timeStart = new Date().getTime();
+    await fetch(`${location.protocol}//${host}`, { mode: 'no-cors' });
+    const timeEnd = new Date().getTime();
+    return timeEnd - timeStart;
 };
 
-const buildTable = () =>
+const pingSample = async (host, count) => {
+    let total = 0;
+    for (let i = 0; i < count; i++) {
+        total += await ping(host);
+    }
+    return total / count;
+}
+
+const updateClasses = () => {
+    for (const cell of document.querySelectorAll("td.col-latency")) {
+        cell.classList.toggle("loading", cell.textContent === ' ');
+    }
+}
+
+const updateCellIdx = (index, value) => {
+    $("#tb-ping-results").bootstrapTable("updateCell", {
+        field: "latency",
+        index,
+        value,
+    });
+    updateClasses();
+}
+
+const updateCell = (element, value) => updateCellIdx(element[0].dataset.index, value);
+
+const worldUrl = (num) => "oldschool" + (parseInt(num) - 300) + ".runescape.com";
+
+const pingAllWorlds = () => {
+    const len = document.querySelectorAll("td.col-latency").length;
+    for (let i = 0; i < len; i++) {
+        // every click will rerender the table so we need to reselect
+        const cells = document.querySelectorAll("td.col-latency");
+        cells[i].click();
+    }
+};
+
+$(document).ready(() => { 
     $("#tb-ping-results").bootstrapTable({
         search: true,
         striped: true,
-        columns: [{
-            sortable: true,
-            field: "world",
-            title: "World",
-            class: "col-world",
-        }, {
-            sortable: true,
-            field: "type",
-            title: "Type",
-            class: "col-type",
-        }, {
-            sortable: true,
-            field: "members",
-            title: "Members",
-            class: "col-members",
-        }, {
-            sortable: true,
-            field: "location",
-            title: "Location",
-            class: "col-location",
-        }, {
-            sortable: true,
-            field: "latency",
-            title: "Latency",
-            class: "col-latency",
-        }],
-        data: worldData.map((w) => ({
-            world: w["id"],
-            type: w["type"],
-            members: w["members"],
-            location: w["location"],
-            latency: "",
-        })),
-        onClickRow: async (row, element, field) => {
-            const worldID = row["world"];
-            const worldUrl = "oldschool" + (parseInt(worldID) - 300) + ".runescape.com";
-            const time = await ping(worldUrl);
-            $("#tb-ping-results").bootstrapTable("updateCell", {
-                index: element[0].dataset.index,
-                field: "latency",
-                value: time,
-            });
+        columns,
+        data,
+        onClickRow: async (row, element) => {
+            updateCell(element, " ");
+            const time = await pingSample(worldUrl(row["world"]), 10);
+            updateCell(element, `${time}ms`);
         },
     });
 
-$(document).ready(function () {
-    buildTable();
+    setTimeout(pingAllWorlds, 100);
 });
+
